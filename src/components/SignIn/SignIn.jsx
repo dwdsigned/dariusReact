09a535e384a6be29e8c6e141f7c2cff17a249837
Darius Williams <dariustwilliams668@gmail.com>
2024-01-14 09:03:23 -0500

    added Tachyons form with its styling in the jsx and css files

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -3,33 +3,50 @@ import './SignIn.css';
 
 const SignIn = () => {
     return (
-        <div className="SignIn">
-            <main className="">
-                <form className="">
-                    <fieldset id="sign_up" className="">
-                        <legend className="">Sign In</legend>
-                        <div className="">
-                            <label className="" htmlFor="email-address">Email</label>
-                            <input className="" type="email" name="email-address" id="email-address" />
-                        </div>
-                        <div className="">
-                            <label className="" htmlFor="password">Password</label>
-                            <input className="" type="password" name="password" id="password" />
-                        </div>
-                        <label className="">
-                            <input type="checkbox" /> Remember me
-                        </label>
-                    </fieldset>
-                    <div className="">
-                        <input className="" type="submit" value="Sign in" />
-                    </div>
-                    <div className="">
-                        <a href="#0" className="">Sign up</a>
-                        <a href="#0" className="">Forgot your password?</a>
-                    </div>
-                </form>
-            </main>
-        </div>
+        <main className="pa4 black-80 centerReact">
+  <form className="measure center">
+    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+      <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+      <div className="mt3">
+        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
+      </div>
+      <div className="mv3">
+        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
+      </div>
+      <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label>
+    </fieldset>
+    <div className="">
+      <input className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+    </div>
+    <div className="lh-copy mt3">
+      <a href="#0" className="f6 link dim black db">Sign up</a>
+      <a href="#0" className="f6 link dim black db">Forgot your password?</a>
+    </div>
+  </form>
+</main>
+        // <div className="SignIn">
+        //     <main className="">
+        //         <form className="">
+        //             <fieldset id="sign_up" className="">
+        //                 <legend className="legend">Sign In</legend>
+        //                 <div className="">
+        //                     <label className="" htmlFor="email-address">Email</label>
+        //                     <input className="" type="email" name="email-address" id="email-address" />
+        //                 </div>
+        //                 <div className="">
+        //                     <label className="" htmlFor="password">Password</label>
+        //                     <input className="" type="password" name="password" id="password" />
+        //                 </div>
+        //                 <div className="">
+        //                 <input className="" type="submit" value="Sign in" />
+        //                 <a href="#0" className="">Register</a>
+        //             </div>
+        //             </fieldset>
+        //         </form>
+        //     </main>
+        // </div>
     );
 }
 
